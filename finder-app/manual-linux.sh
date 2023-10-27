08077ab0099a40da35f6e5449390fe0859d6c50e
youssef456 <yousefnagy2@gmail.com>
2023-10-27 10:31:02 +0200

    test5

--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -11,7 +11,6 @@ KERNEL_VERSION=v5.1.10
 BUSYBOX_VERSION=1_33_1
 FINDER_APP_DIR=$(realpath $(dirname $0))
 ARCH=arm64
-#="$(readlink -f /home/youssef/gcc-arm-10.2-2020.11-x86_64-aarch64-none-linux-gnu/bin/)"
 CROSS_COMPILER_PATH=/home/youssef/gcc-arm-10.2-2020.11-x86_64-aarch64-none-linux-gnu/bin/
 CROSS_COMPILE="${CROSS_COMPILER_PATH}aarch64-none-linux-gnu-"
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
@@ -79,17 +78,15 @@ then
     cd busybox
     git checkout ${BUSYBOX_VERSION}
     # TODO:  Configure busybox
-    make ARCH=arm64 CROSS_COMPILE="${CROSS_COMPILE}" defconfig
+    make ARCH=arm64 CROSS_COMPILE=aarch64-none-linux-gnu- defconfig
 else
     cd busybox
 fi
 
 # TODO: Make and install busybox
 cd ${OUTDIR}/busybox
-#make ARCH=arm64 CROSS_COMPILE="${CROSS_COMPILE}" -j4
-#make ARCH=arm64 CROSS_COMPILE="${CROSS_COMPILE}" install
-make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE}
-make CONFIG_PREFIX=${OUTDIR}/rootfs ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} install
+make ARCH=${ARCH} CROSS_COMPILE=aarch64-none-linux-gnu-
+make CONFIG_PREFIX=${OUTDIR}/rootfs ARCH=${ARCH} CROSS_COMPILE=aarch64-none-linux-gnu- install
 
 cd ${OUTDIR}/rootfs/
 
@@ -111,7 +108,7 @@ sudo mknod -m 666 ${OUTDIR}/rootfs/dev/console c 5 1
 # TODO: Clean and build the writer utility
 cd ${FINDER_APP_DIR}
 make clean
-make ARCH=arm64 CROSS_COMPILE=${CROSS_COMPILE}
+make ARCH=arm64 CROSS_COMPILE=aarch64-none-linux-gnu-
 
 # TODO: Copy the finder related scripts and executables to the /home directory
 # on the target rootfs
