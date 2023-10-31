82f99808c51be9a7cf0b07bb938a85960f99b00a
youssef456 <yousefnagy2@gmail.com>
2023-10-31 15:51:40 +0200

    assignment4-part2

--- finder-app/finder-test.sh
+++ finder-app/finder-test.sh
@@ -64,7 +64,7 @@ echo "Current working directory is: $current_directory"
 
 files=$(ls)
 echo "files in directory is: $files"
-chmod +x ./finder.sh
+#chmod +x ./finder.sh
 
 #. ./finder.sh "$WRITEDIR" "$WRITESTR"
 OUTPUTSTRING=$(. ./finder.sh "$WRITEDIR" "$WRITESTR")
