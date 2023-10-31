40fd6475a75ff31888adbae8cada6a315498b706
youssef456 <yousefnagy2@gmail.com>
2023-10-31 11:19:28 +0200

    assignment4-part2

--- finder-app/finder-test.sh
+++ finder-app/finder-test.sh
@@ -8,8 +8,8 @@ set -u
 NUMFILES=10
 WRITESTR="AELD_IS_FUN"
 WRITEDIR="/tmp/aeld-data"
-username=$(cat ./conf/username.txt)
-#username=$(cat /etc/finder-app/conf/username.txt)
+#username=$(cat conf/username.txt)
+username=$(cat /etc/finder-app/conf/username.txt)
 if [ $# -lt 3 ]
 then
 	echo "Using default value '${WRITESTR}' for string to write"
@@ -32,8 +32,8 @@ echo "Writing ${NUMFILES} files containing string '${WRITESTR}' to '${WRITEDIR}'
 rm -rf "${WRITEDIR}"
 
 # create $WRITEDIR if not assignment1
-assignment=$(cat ./conf/assignment.txt)
-#assignment=$(cat etc/finder-app/conf/assignment.txt)
+#assignment=$(cat conf/assignment.txt)
+assignment=$(cat etc/finder-app/conf/assignment.txt)
 if [ "$assignment" != 'assignment1' ]
 then
 	mkdir -p "$WRITEDIR"
