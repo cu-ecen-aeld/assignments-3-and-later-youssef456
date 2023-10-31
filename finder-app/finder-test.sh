5b8109b4133e70766df5247e433c9d78db90c9c7
youssef456 <yousefnagy2@gmail.com>
2023-10-31 16:20:27 +0200

    assignment4-part2

--- finder-app/finder-test.sh
+++ finder-app/finder-test.sh
@@ -64,11 +64,11 @@ echo "Current working directory is: $current_directory"
 
 files=$(ls)
 echo "files in directory is: $files"
-#chmod +x ./finder.sh
+chmod +x ./finder.sh
 
 #. ./finder.sh "$WRITEDIR" "$WRITESTR"
-#OUTPUTSTRING=$(. ./finder.sh "$WRITEDIR" "$WRITESTR")
-OUTPUTSTRING=$(finder.sh "$WRITEDIR" "$WRITESTR")
+OUTPUTSTRING=$(. ./finder.sh "$WRITEDIR" "$WRITESTR")
+OUTPUTSTRING=$(/etc/finder-app/finder.sh "$WRITEDIR" "$WRITESTR")
 echo "Output from finder program: $OUTPUTSTRING"
 
 # Remove temporary directories
