6f185e98e8860377d24357ebe6875ec3f0dd5ef7
youssef456 <yousefnagy2@gmail.com>
2023-11-24 05:07:32 +0200

    assignment-8-fix

--- aesd-char-driver/main.c
+++ aesd-char-driver/main.c
@@ -74,7 +74,7 @@ ssize_t aesd_read(struct file *filp, char __user *buf, size_t count, loff_t *f_p
 
     return retval;
 
-
+}
 ssize_t aesd_write(struct file *filp, const char __user *buf, size_t count,
                    loff_t *f_pos)
 {
