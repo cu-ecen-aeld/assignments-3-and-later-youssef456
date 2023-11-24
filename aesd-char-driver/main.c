335e765a14aefb77a7446b88a9bd869a5a51b922
youssef456 <yousefnagy2@gmail.com>
2023-11-24 05:04:00 +0200

    assignment-8-fix

--- aesd-char-driver/main.c
+++ aesd-char-driver/main.c
@@ -30,7 +30,7 @@ struct aesd_dev aesd_device;
 int aesd_open(struct inode *inode, struct file *filp)
 {
     PDEBUG("open");
-    aesd_dev *dev;
+    struct aesd_dev *dev;
     dev = container_of(inode->i_cdev, struct aesd_dev, cdev);
     filp->private_data = dev;
     return 0;
