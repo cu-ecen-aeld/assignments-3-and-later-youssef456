72b7490c6b9f511b0aaad6f90db345adbabc74d1
youssef456 <yousefnagy2@gmail.com>
2023-11-27 17:11:17 +0200

    assignment-9

--- server/aesdsocket.c
+++ server/aesdsocket.c
@@ -72,7 +72,7 @@ void handle_connection(int client_socket) {
     char buffer[MAX_PACKET_SIZE];
     ssize_t bytes_received;
 
-    while ((bytes_received = recv(client_socket, buffer, sizeof(buffer), 0)) > 0) {
+     while ((bytes_received = recv(client_socket, buffer, sizeof(buffer), 0)) > 0) {
 #ifdef USE_AESD_CHAR_DEVICE
         if (strncmp(buffer, "AESDCHAR_IOCSEEKTO:", 19) == 0) {
             unsigned int x, y;
@@ -87,10 +87,17 @@ void handle_connection(int client_socket) {
                 break;
             }
         } else {
+            // Open the character device when needed
+            if ((aesd_char_fd = open("/dev/aesdchar", O_RDWR)) == -1) {
+                syslog(LOG_ERR, "Failed to open /dev/aesdchar: %s", strerror(errno));
+                break;
+            }
+
             pthread_mutex_lock(&data_mutex);
             if (write(data_fd, buffer, bytes_received) == -1) {
                 syslog(LOG_ERR, "Failed to write data to %s: %s", DATA_FILE, strerror(errno));
                 pthread_mutex_unlock(&data_mutex);
+                close(aesd_char_fd);  // Close the character device if write fails
                 break;
             }
             pthread_mutex_unlock(&data_mutex);
@@ -101,9 +108,13 @@ void handle_connection(int client_socket) {
             while ((bytes_read = read(data_fd, read_buffer, sizeof(read_buffer))) > 0) {
                 if (send(client_socket, read_buffer, bytes_read, 0) == -1) {
                     syslog(LOG_ERR, "Failed to send data to the client: %s", strerror(errno));
+                    close(aesd_char_fd);  // Close the character device if send fails
                     break;
                 }
             }
+
+            // Close the character device after processing
+            close(aesd_char_fd);
         }
 #else
         pthread_mutex_lock(&data_mutex);
