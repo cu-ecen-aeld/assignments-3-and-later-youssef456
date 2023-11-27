591dc17a29197e7d3ba3965e5135299f9838f99b
youssef456 <yousefnagy2@gmail.com>
2023-11-27 15:28:21 +0200

    assignment-9

--- server/aesdsocket-start-stop.sh
+++ server/aesdsocket-start-stop.sh
@@ -12,17 +12,18 @@ DAEMON=./aesdsocket
 DAEMON_ARGS="-d"  # Add any other necessary arguments here
 NAME=aesdsocket
 DESC="AESD Socket Application"
+PIDFILE=/var/run/aesdsocket.pid
 
 case "$1" in
   start)
     echo "Starting $DESC: $NAME"
-    #start-stop-daemon --start --background --make-pidfile --pidfile /var/tmp/$NAME.pid --exec $DAEMON -- $DAEMON_ARGS
-    start-stop-daemon -S -n aesdsocket -a /usr/bin/aesdsocket -- -d
+    start-stop-daemon --start --background --make-pidfile --pidfile $PIDFILE --exec $DAEMON -- $DAEMON_ARGS
+    ##start-stop-daemon -S -n aesdsocket -a /usr/bin/aesdsocket -- -d
     ;;
   stop)
     echo "Stopping $DESC: $NAME"
-    #start-stop-daemon --stop --signal TERM --pidfile /var/tmp/$NAME.pid
-    start-stop-daemon -K -n aesdsocket
+    start-stop-daemon --stop --pidfile $PIDFILE
+    ##start-stop-daemon -K -n aesdsocket
     ;;
   restart)
     $0 stop
