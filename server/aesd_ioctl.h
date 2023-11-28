ae60cca148b63d2f38b3c5d162099bbb320298f2
youssef456 <yousefnagy2@gmail.com>
2023-11-28 13:58:28 +0200

    assignment-9

--- server/aesd_ioctl.h
+++ server/aesd_ioctl.h
@@ -1,22 +1,46 @@
+/*
+ * aesd_ioctl.h
+ *
+ *  Created on: Oct 23, 2019
+ *      Author: Dan Walkes
+ *
+ *  @brief Definitins for the ioctl used on aesd char devices for assignment 9
+ */
+
 #ifndef AESD_IOCTL_H
 #define AESD_IOCTL_H
 
-#include <linux/ioctl.h>
+#ifdef __KERNEL__
+#include <asm-generic/ioctl.h>
 #include <linux/types.h>
+#else
+#include <sys/ioctl.h>
+#include <stdint.h>
+#endif
 
-// Define an arbitrary ioctl magic number
-#define AESD_IOC_MAGIC 0x16
-
-// Define a write command structure for seek ioctl
+/**
+ * A structure to be passed by IOCTL from user space to kernel space, describing the type
+ * of seek performed on the aesdchar driver
+ */
 struct aesd_seekto {
+    /**
+     * The zero referenced write command to seek into
+     */
     uint32_t write_cmd;
+    /**
+     * The zero referenced offset within the write
+     */
     uint32_t write_cmd_offset;
 };
 
-// Define the ioctl command for seek operation
-#define AESDCHAR_IOCSEEKTO _IOWR(AESD_IOC_MAGIC, 1, struct aesd_seekto)
+// Pick an arbitrary unused value from https://github.com/torvalds/linux/blob/master/Documentation/userspace-api/ioctl/ioctl-number.rst
+#define AESD_IOC_MAGIC 0x16
 
-// Define the maximum number of ioctl commands for bounds checking
+// Define a write command from the user point of view, use command number 1
+#define AESDCHAR_IOCSEEKTO _IOWR(AESD_IOC_MAGIC, 1, struct aesd_seekto)
+/**
+ * The maximum number of commands supported, used for bounds checking
+ */
 #define AESDCHAR_IOC_MAXNR 1
 
 #endif /* AESD_IOCTL_H */
