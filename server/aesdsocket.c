57dd23b1643e3e7c93a5a6422ed6e7083490c4ff
youssef456 <yousefnagy2@gmail.com>
2023-11-28 02:50:02 +0200

    assignment-9

--- server/aesdsocket.c
+++ server/aesdsocket.c
@@ -45,24 +45,17 @@ time_t last_timestamp;
 void signal_handler(int signo) {
     if (signo == SIGINT || signo == SIGTERM) {
         syslog(LOG_INFO, "Caught signal, exiting");
-
         pthread_mutex_lock(&data_mutex);
-
         if (data_fd != -1)
             close(data_fd);
-
 #if USE_AESD_CHAR_DEVICE == 1
         if (aesd_char_fd != -1)
             close(aesd_char_fd);
 #endif
-
         if (listen_socket != -1)
             close(listen_socket);
-
         remove(PID_FILE);
-
         pthread_mutex_unlock(&data_mutex);
-
         closelog();
         exit(EXIT_SUCCESS);
     }
@@ -79,7 +72,7 @@ void handle_connection(int client_socket) {
         close(client_socket);
         return;
     }
-    
+
     while ((bytes_received = recv(client_socket, buffer, sizeof(buffer), 0)) > 0) {
         // Add a debugging statement to print the received data
         printf("Received data: %.*s\n", (int)bytes_received, buffer);
@@ -143,7 +136,7 @@ void handle_connection(int client_socket) {
     }
     // Close the character device after processing
     close(aesd_char_fd);
-    
+
     syslog(LOG_INFO, "Closed connection");
     close(client_socket);
 }
@@ -218,90 +211,7 @@ void* timestamp_updater(void* arg) {
 }
 
 int main(int argc, char *argv[]) {
-    if (argc > 1 && strcmp(argv[1], "-d") == 0) {
-        int pid_fd = open(PID_FILE, O_RDONLY);
-        if (pid_fd != -1) {
-            char pid_str[16];
-            ssize_t n = read(pid_fd, pid_str, sizeof(pid_str));
-            if (n > 0) {
-                pid_t existing_pid = atoi(pid_str);
-                if (kill(existing_pid, 0) == 0) {
-                    syslog(LOG_ERR, "Daemon is already running with PID %d.", existing_pid);
-                    close(pid_fd);
-                    exit(EXIT_FAILURE);
-                }
-            }
-            close(pid_fd);
-        }
-
-        pid_t pid = fork();
-        if (pid < 0) {
-            perror("Fork failed");
-            return EXIT_FAILURE;
-        } else if (pid > 0) {
-            return EXIT_SUCCESS;
-        }
-
-        setsid();
-        umask(0);
-        chdir("/");
-        close(STDIN_FILENO);
-        close(STDOUT_FILENO);
-        close(STDERR_FILENO);
-        write_pid_file();
-    }
-
-    openlog("aesdsocket", LOG_PID, LOG_DAEMON);
-    signal(SIGINT, signal_handler);
-    signal(SIGTERM, signal_handler);
-
-    pthread_t timestamp_thread;
-
-    if (pthread_mutex_init(&data_mutex, NULL) != 0) {
-        syslog(LOG_ERR, "Failed to initialize the data_mutex");
-        return EXIT_FAILURE;
-    }
-
-    if (pthread_mutex_init(&timestamp_mutex, NULL) != 0) {
-        syslog(LOG_ERR, "Failed to initialize the timestamp_mutex");
-        return EXIT_FAILURE;
-    }
-
-    last_timestamp = time(NULL);
-
-    if (pthread_create(&timestamp_thread, NULL, timestamp_updater, NULL) != 0) {
-        syslog(LOG_ERR, "Failed to create the timestamp updater thread");
-        return EXIT_FAILURE;
-    }
-
-    struct sockaddr_in server_addr;
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_port = htons(PORT);
-    server_addr.sin_addr.s_addr = INADDR_ANY;
-
-    if ((listen_socket = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
-        syslog(LOG_ERR, "Failed to create socket: %s", strerror(errno));
-        return EXIT_FAILURE;
-    }
-
-    if (bind(listen_socket, (struct sockaddr *)&server_addr, sizeof(server_addr)) == -1) {
-        syslog(LOG_ERR, "Failed to bind to port %d: %s", PORT, strerror(errno));
-        return EXIT_FAILURE;
-    }
-
-    if (listen(listen_socket, 5) == -1) {
-        syslog(LOG_ERR, "Failed to listen on the socket: %s", strerror(errno));
-        return EXIT_FAILURE;
-    }
-
-#if USE_AESD_CHAR_DEVICE == 1
-    if ((aesd_char_fd = open("/dev/aesdchar", O_RDWR)) == -1) {
-        syslog(LOG_ERR, "Failed to open /dev/aesdchar: %s", strerror(errno));
-        exit(EXIT_FAILURE);
-    }
-#endif
-
-    data_fd = open(DATA_FILE, O_RDWR | O_CREAT, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
+    // ... (unchanged part)
 
     while (1) {
         int client_socket = accept(listen_socket, NULL, NULL);
@@ -328,6 +238,7 @@ int main(int argc, char *argv[]) {
 
         pthread_detach(thread);
     }
+
 #if USE_AESD_CHAR_DEVICE == 1
     if (aesd_char_fd != -1)
         close(aesd_char_fd);
